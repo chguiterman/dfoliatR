04f7404a76759afc0ab7a02b9569d07da9b16316
Chris Guiterman <chguiterman@email.arizona.edu>
2018-08-05 09:16:58 -0700

    Begin defoliate_trees function

---
+++ R/defoliate_trees.R
@@ -0,0 +1,19 @@
+#' Measure and defoliation events in host trees
+#' @description Conduct host-nonhost comparison
+#'
+#' @param host_tree a data.frame rwl object containing the tree-level growth series for all
+#' host trees to be compared to the non-host chronology
+#' @param nonhost_chron a data.frame rwl object comtaining a single non-host chronology
+#' @param duration_years the mimimum number of years in which to consider a defolation event
+#' @param max_reduction the minimum level of tree growth to be considered in defoliation
+#'
+#' @return a list object with elements containing data.frame rwl objects of the host and non-host series, corrected
+#'
+
+defoliate_trees <- function(host_tree, nonhost_chron, duration_years = 8, max_reduction = -1.28) {
+  if(ncol(nonhost_chron) > 1) stop("nonhost_chron can only contain 1 series")
+  if(max_reduction > 0) max_reduction <- max_reduction * -1
+  nseries <- ncol(host_tree)
+  lapply(seq_len(nseries), function(i){
+    input_series <- na.omit(dplR::combine.rwl(host_tree[, i, drop=FALSE], nonhost_chron))
+    corrected_series <- correct_host_series
