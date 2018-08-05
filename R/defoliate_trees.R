06b00ad42f1b3f3934ac80852a5c860e9469a7ec
Chris Guiterman <chguiterman@email.arizona.edu>
2018-08-05 09:29:04 -0700

    specify data,frames and add to description

--- R/defoliate_trees.R
+++ R/defoliate_trees.R
@@ -1,5 +1,5 @@
 #' Measure and defoliation events in host trees
-#' @description Conduct host-nonhost comparison
+#' @description Conduct host/nonhost comparison to identify defoliation events
 #'
 #' @param host_tree a data.frame rwl object containing the tree-level growth series for all
 #' host trees to be compared to the non-host chronology
@@ -13,7 +13,16 @@
 defoliate_trees <- function(host_tree, nonhost_chron, duration_years = 8, max_reduction = -1.28) {
   if(ncol(nonhost_chron) > 1) stop("nonhost_chron can only contain 1 series")
   if(max_reduction > 0) max_reduction <- max_reduction * -1
+  host_tree <- data.frame(host_tree)
+  nonhost_chron <- data.frame(nonhost_chron)
   nseries <- ncol(host_tree)
   lapply(seq_len(nseries), function(i){
     input_series <- na.omit(dplR::combine.rwl(host_tree[, i, drop=FALSE], nonhost_chron))
-    corrected_series <- correct_host_series
+    corrected_series <- correct_host_series(input_series)
+    defoliated_series <- id_defoliation(corrected_series, duration_years = duration_years, max_reduction = max_reduction)
+    return(defoliated_series)
+    }
+  )
+}
+
+
