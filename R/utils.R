d8049830a5af18a2fc24e99e15f1f1567b084303
Chris Guiterman <chguiterman@email.arizona.edu>
2018-12-04 08:39:26 -0500

    big thing: prevent overlapping defoliations in id_defoliation. Small thing: set negative corrrected values to zero in correct_host_series

--- R/utils.R
+++ R/utils.R
@@ -15,8 +15,11 @@ correct_host_series <- function(input_series){
   h_sd <- stats::sd(input_series[, 1])
   nh_sd <- stats::sd(input_series[, 2])
   nh_mean <- mean(input_series[, 2])
+  # To Do: Add provision to prevent large non-host values from swaying results
+  # See outbreak.txt for description of fractional power.
   input_series[, 3] <- (input_series[, 2] - nh_mean) * (h_sd / nh_sd)
   input_series[, 4] <- input_series[, 1] - input_series[, 3]
+  input_series[, 4] <- replace(input_series[, 4], input_series[, 4] < 0, 0) # set negative #s to zero
   input_series[, 5] <- scale(input_series[, 4])
   names(input_series) <- c(nms, nam1, nam2, nam3)
   return(input_series)
@@ -44,19 +47,34 @@ id_defoliation <- function(input_series, duration_years = 8, max_reduction = -1.
   if (0 %in% newindex) starts = c(1,starts)
   deps <- data.frame(cbind(starts, ends))
   deps$length <- deps$ends - deps$starts + 1
+  input_series$defol_status = NA
   for(y in 1:nrow(deps)){
     bb <- input_series[deps$starts[y] : deps$ends[y], 5]
     max.red <- deps$starts[y] + which.min(bb) - 1
     if(input_series[max.red, 5] > max_reduction) next  # Includes setting for max growth reduction
     dep.seq <- deps$starts[y] : deps$ends[y]
+    prev.flag <- FALSE
     if(y > 1){
-      if(min(dep.seq) - deps$ends[y - 1] == 2) dep.seq <- c(deps$starts[y - 1] : max(dep.seq))
+      if(min(dep.seq) - deps$ends[y - 1] == 2) {
+        if(! any(input_series[deps$starts[y - 1] : deps$ends[y - 1], ]$defol_status  %in% c("defoliated", "max_defoliation"))){
+          dep.seq <- c(deps$starts[y - 1] : max(dep.seq))
+          prev.flag <- TRUE
+        }
+      }
     }
     if(y < nrow(deps)){
-      if(deps$starts[y + 1] - max(dep.seq) == 2) dep.seq <- c(min(dep.seq) : deps$ends[y + 1])
+      if(deps$starts[y + 1] - max(dep.seq) == 2){
+        if(min(input_series[dep.seq, 5]) > min(input_series[deps$starts[y + 1] : deps$ends[y + 1], 5]))  next
+        if((! prev.flag) & (y < nrow(deps) - 1)){
+          if(deps$starts[y + 2] - deps$ends[y + 1] == 2){
+            if(min(input_series[dep.seq, 5]) > min(input_series[deps$starts[y + 2] : deps$ends[y + 2], 5]))  next
+          }
+        }
+        dep.seq <- c(min(dep.seq) : deps$ends[y + 1])
+      }
     }
     if(length(dep.seq) < duration_years) next # Includes setting for min defoliation duration
-    if(min(input_series[dep.seq, 5]) != input_series[max.red, 5]) next
+    # if(min(input_series[dep.seq, 5]) != input_series[max.red, 5]) next
     input_series[dep.seq, 6] <- "defoliated"
     input_series[max.red, 6] <- "max_defoliation"
   }
