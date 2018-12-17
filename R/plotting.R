05feed111f7b44a71d0f5e5fb8694353c60cb498
Chris Guiterman <chguiterman@email.arizona.edu>
2018-12-17 14:58:19 -0500

    change y intercept for plotting cor_index

--- R/plotting.R
+++ R/plotting.R
@@ -17,9 +17,11 @@ plot_defol <- function(x, disp_index = "norm_index", col_defol = 'black') {
   defol_events <- x[!is.na(x$defol_status), ]
   p <- ggplot2::ggplot(data = x, ggplot2::aes_string(x="year", y=disp_index, group="series"))
   p <- (p + ggplot2::geom_hline(yintercept = y_intercept) + ggplot2::geom_line())
-  p <- (p + ggplot2::geom_bar(data = defol_events,
-                              ggplot2::aes_string(x="year", y=disp_index), stat="identity",
-                              fill = col_defol))
+  p <- (p + ggplot2::geom_segment(data = defol_events,
+                              ggplot2::aes_string(x="year", xend="year",
+                                                  y=y_intercept, yend=disp_index),
+                              colour = col_defol,
+                              size=2))
   p <- (p + ggplot2::facet_grid(series ~ .))
   p <- (p + ggplot2::theme_bw())
   p
@@ -31,13 +33,14 @@ plot_defol <- function(x, disp_index = "norm_index", col_defol = 'black') {
 #' @param disp_index Identify the timeseries index to plot. Defaults to \code{mean_norm_index}. The only other option is \code{mean_cor_index}.
 #'
 #' @export
-
 plot_outbreak <- function(x, disp_index = "mean_norm_index"){
   if(!is.outbreak(x)) stop("'x' must be an 'outbreak' object")
   if(! (disp_index == "mean_cor_index" | disp_index == "mean_norm_index")) {
     warning("Displaying the 'mean_norm_index'")
     disp_index <- "mean_norm_index"
   }
+  if(disp_index == "mean_norm_index") y_intercept <- 0
+  else y_intercept <- 1
   outbrk_events <- x[! is.na(x$outbreak_status), ]
   # setup plot
   p <- ggplot2::ggplot(data = x, ggplot2::aes_string(x="year"))
@@ -47,9 +50,12 @@ plot_outbreak <- function(x, disp_index = "mean_norm_index"){
   # top plot
   index <- p +
     ggplot2::geom_vline(xintercept = minor_labs, colour="grey50") +
-    ggplot2::geom_hline(yintercept = 0, colour = "grey80") +
+    ggplot2::geom_hline(yintercept = y_intercept, colour = "grey80") +
     ggplot2::geom_line(ggplot2::aes_string(y=disp_index)) +
-    ggplot2::geom_bar(data = outbrk_events, ggplot2::aes_string(x="year", y=disp_index), stat="identity") +
+    ggplot2::geom_segment(data = outbrk_events,
+                          ggplot2::aes_string(x="year", xend="year",
+                                              y=y_intercept, yend=disp_index),
+                          size=2) +
     ggplot2::scale_y_continuous(name = "Growth index") +
     ggpubr::theme_pubr() +
     ggplot2::theme(plot.margin = ggplot2::unit(c(0.1, 0, 0, 0), "cm"),
