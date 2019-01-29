16dcf321b5b6a85bc0640ee75b314ba80292b7b5
Chris Guiterman <chguiterman@email.arizona.edu>
2019-01-28 20:10:09 -0500

    document data

---
+++ R/data.R
@@ -0,0 +1,30 @@
+#' East Fork Jemez River Douglas-fir
+#'
+#' Western spruce budworm host series
+#'
+#' @format An `rwl` object with 15 tree-level series, standardized in ARSTAN.
+#'   Dates range from 1776-1987.
+"ef_h"
+
+#' East Fork Jemez area ponderosa pine
+#'
+#' Non-host pair chronology for East Fork Douglas-fir `ef_h`
+#'
+#' @format An `rwl` chronology object with 1 series, 1612-1987. Standardized in
+#'   ARSTAN.
+"ef_nh"
+
+#' Demi John Douglas-fir
+#'
+#' Western spruce budworm host series
+#'
+#' @format An `rwl` object with 17 tree-level series, standardized in ARSTAN.
+#'   Dates range from 1620-1997.
+"dmj_h"
+
+#' Demi John area ponderosa pine
+#'
+#' Non-host pair to Demi John Douglas-fir `dmj_h`
+#'
+#' @format An `rwl` object with 1 series, 1675-1997.
+"dmj_h"
