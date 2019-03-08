d8bbeb38a50a7358cb214088724fd4ae2f23cc1b
Chris Guiterman <chguiterman@email.arizona.edu>
2019-03-08 10:41:01 -0500

    get dmj outputs

--- inst/extdata/dmj_read.R
+++ inst/extdata/dmj_read.R
@@ -1,12 +1,21 @@
-library(dplR)
 
 # Demi John host series
-dmj_h <- read.compact('inst/extdata/DMJDFARS.TRE')
+dmj_h <- dplR::read.compact('inst/extdata/DMJDFARS.TRE')
 
 usethis::use_data(dmj_h, overwrite = TRUE)
 
 # Nonhost chronology for Eastfork
-wir <- read.crn('inst/extdata/Wirpipo.crn')
+wir <- dplR::read.crn('inst/extdata/Wirpipo.crn')
 dmj_nh <- wir[, 1, drop = FALSE]
 
 usethis::use_data(dmj_nh, overwrite = TRUE)
+
+# Run defoliate_trees
+dmj_defol <- dfoliatR::defoliate_trees(dmj_h, dmj_nh, series_end_event = TRUE)
+
+usethis::use_data(dmj_defol, overwrite = TRUE)
+
+# Run outbreak on defol
+dmj_obr <- dfoliatR::outbreak(dmj_defol)
+
+usethis::use_data(dmj_obr, overwrite = TRUE)
