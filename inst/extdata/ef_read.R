8db3b902568dc2d587ede77a96af968e4652d041
Chris Guiterman <chguiterman@email.arizona.edu>
2019-03-08 10:40:34 -0500

    specify packages

--- inst/extdata/ef_read.R
+++ inst/extdata/ef_read.R
@@ -1,22 +1,22 @@
-library(dplR)
+
 
 # East fork host series
-ef_h <- read.compact('inst/extdata/EFKDF2.TRE')
+ef_h <- dplR::read.compact('inst/extdata/EFKDF2.TRE')
 
 usethis::use_data(ef_h, overwrite = TRUE)
 
 # Nonhost chronology for Eastfork
-bac <- read.compact('inst/extdata/BAC2.CRN')
+bac <- dplr::read.compact('inst/extdata/BAC2.CRN')
 ef_nh <- bac[, 1, drop = FALSE]
 
 usethis::use_data(ef_nh, overwrite = TRUE)
 
 # Run defoliate_trees
-ef_defol <- defoliate_trees(ef_h, ef_nh, series_end_event = TRUE)
+ef_defol <- dfoliatR::defoliate_trees(ef_h, ef_nh, series_end_event = TRUE)
 
 usethis::use_data(ef_defol, overwrite = TRUE)
 
 # Run outbreak on defol
-ef_obr <- outbreak(ef_defol)
+ef_obr <- dfoliatR::outbreak(ef_defol)
 
 usethis::use_data(ef_obr, overwrite = TRUE)
